170e4e0e1aeddda781cd65207fc24999b31958c2
xoliio <xoliio@gmail.com>
2020-11-20 14:44:21 -0800

    with Ellaine and Will

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,32 @@
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    fetch(TRAINERS_URL)
+        .then(response => response.json())
+        .then(trainers => showTrainers(trainers))
+})
+
+function showTrainers(trainers){
+    for(trainer of trainers){
+        buildTrainer(trainer)
+    }
+}
+
+function buildTrainer(trainer){
+    newDiv = document.createElement('div')
+    newDiv.className = 'card'
+    newDiv.dataset.id = trainer.id
+    p = document.createElement('p')
+    p.innerText = trainer.name
+    newDiv.appendChild(p)
+    btn = document.createElement('button')
+    btn.dataset.id = trainer.id
+    btn.innerText = 'Add Pokemon'
+    newDiv.appendChild(btn)
+    list = document.createElement('ul')
+    
+    
+}
